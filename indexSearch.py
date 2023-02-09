f84459bab22308f50aa3400431be6e630bcf4603
Anikhet Mulky <51974582+Anikhet@users.noreply.github.com>
2023-02-09 12:30:12 -0500

    First commit

---
+++ indexSearch.py
@@ -0,0 +1,54 @@
+"""
+file: indexSearch.py
+language: python3
+author: Anikhet Mulky , am9559@g.rit.edu, Matthew Thai, tqt6351@rit.edu
+"""
+
+
+def bin_search(data, left, right):
+    """
+    Binary search algorithm.
+    :param data: Array to search on.
+    :param left: leftmost index
+    :param right: rightmost index
+    """
+
+    if left > right:  # A[k] = k was not found in the array.
+        print("FALSE")
+        quit()
+    mid_index = (left + right) // 2
+
+    if data[mid_index] == mid_index:
+        print("TRUE")
+        quit()
+    if data[mid_index] > mid_index:
+        return bin_search(data, left, mid_index - 1)  # Half of the array is discarded every time.
+    else:
+        return bin_search(data, mid_index + 1, right)
+
+
+def read_input():
+    """
+    Reads input.
+    """
+    num = int(input())
+    lst = list(map(int, input().split()))
+    data = [0] * (len(lst) + 1)
+    indexing = 1
+    for index in lst: # Indexing it to start from 1
+        data[indexing] = index
+        indexing += 1
+    return data
+
+
+def main():
+    """
+    Main function.
+    """
+    data = read_input()
+    bin_search(data, 1, len(data) - 1)
+    print("FALSE")
+
+
+if __name__ == '__main__':
+    main()
